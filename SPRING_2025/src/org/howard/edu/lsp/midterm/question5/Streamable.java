35aab5ac00bd715f1f75f7a41349c65e4d2ed9db
Jier11 <128205050+Jier11@users.noreply.github.com>
2025-03-20 20:19:48 -0400

    Add files via upload
    New versions

--- SPRING_2025/src/org/howard/edu/lsp/midterm/question5/Streamable.java
+++ SPRING_2025/src/org/howard/edu/lsp/midterm/question5/Streamable.java
@@ -2,6 +2,7 @@ package org.howard.edu.lsp.midterm.question5;
 
 /**
  * Interface for streamable.
+ * reference: https://stackoverflow.com/questions/48839053/how-to-use-multiple-classes-in-java-in-one-file
  */
 public interface Streamable {
     void play();
