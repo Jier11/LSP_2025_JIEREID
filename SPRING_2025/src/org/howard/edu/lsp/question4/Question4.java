02c0a1255dc11617020d85969f2c3b77cdbcdb1a
Jier11 <128205050+Jier11@users.noreply.github.com>
2025-04-24 19:54:51 -0400

    Add files via upload

--- SPRING_2025/src/org/howard/edu/lsp/question4/Question4.java
+++ SPRING_2025/src/org/howard/edu/lsp/question4/Question4.java
@@ -2,10 +2,10 @@ package org.howard.edu.lspfinal.question4;
 // reference: https://softwareengineering.stackexchange.com/questions/9219/are-design-patterns-generally-a-force-for-good-or-bad
 public class Question4 {
 // Design patterns can be incredibly useful—when applied correctly, they improve maintainability,
-// readability, and overall developer productivity. Using a pattern when you’ll never use its flexibility
-// just adds needless layers. For example, applying the Strategy pattern when there’s only ever one
-// algorithm creates extra  interfaces, classes, and indirection with no real benefit where a simple
-// method call would be clearer and faster.
+// readability, and overall developer productivity. However if you don't need flexibility, using 
+// a pattern just adds needless layers and complicates things. Applying the Strategy pattern when
+// there’s only ever one algorithm creates extra  interfaces, classes, and indirection with no real
+// benefit where a simple method call would be clearer and faster.
 
 
 
